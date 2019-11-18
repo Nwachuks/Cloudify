ea64dc2a4583d7b299ce7c53dfa086064be19878
Coded Professor <codedprofessor@gmail.com>
2019-11-18 18:46:04 +0100

    Install Pods - SwiftyJSON, Alamofire and SVProgressHUD; tap into phone location using GPS

--- Cloudify/Controller/WeatherViewController.swift
+++ Cloudify/Controller/WeatherViewController.swift
@@ -7,18 +7,17 @@
 //
 
 import UIKit
+import CoreLocation
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     
-    // Constants
+    // Open Weather Map API requirements
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
     let APP_ID = "298850f158b9f072e2560b96dab4afaa"
     
-    //TODO: Declare instance variables here
+    // Core Location Manager
+    let locationManager = CLLocationManager()
     
-    
-    
-    //Pre-linked IBOutlets
     @IBOutlet weak var weatherIcon: UIImageView!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
@@ -27,10 +26,11 @@ class WeatherViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-        //TODO:Set up the location manager here.
-        
-        
+        // TODO:Set up the location manager here.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
         
     }
     
@@ -69,15 +69,28 @@ class WeatherViewController: UIViewController {
     
     
     //MARK: - Location Manager Delegate Methods
-    /***************************************************************/
-    
     
     //Write the didUpdateLocations method here:
-    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+            
+            print("Longitude = \(location.coordinate.longitude), Latitude = \(location.coordinate.latitude)")
+            
+            let longitude = String(location.coordinate.longitude)
+            let latitude = String(location.coordinate.latitude)
+            
+            let params : [String : String] = ["lat" : latitude, "lon" : longitude, "appid" : APP_ID]
+        }
+    }
     
     
     //Write the didFailWithError method here:
-    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+        cityLabel.text = "Location Unavailable"
+    }
     
     
     
---
+++ Podfile
@@ -0,0 +1,17 @@
+platform :ios, '9.0'
+
+target 'Cloudify' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Cloudify
+    pod 'SwiftyJSON'
+    pod 'Alamofire'
+    pod 'SVProgressHUD'
+
+  target 'CloudifyTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
