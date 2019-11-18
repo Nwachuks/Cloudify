70f25e4a1d44c70c968196387c12bc88cbdcf56c
Coded Professor <codedprofessor@gmail.com>
2019-11-18 12:04:29 +0100

    Initial Commit

---
+++ Cloudify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cloudify
+//
+//  Created by Coded Professor on 18/11/2019.
+//  Copyright © 2019 Coded Professor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
